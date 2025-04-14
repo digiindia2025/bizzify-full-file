fdab4d0c281d27439c03c74f4bd9aa534b89669b
digiindia2025 <amantiwariat2005@gmail.com>
2025-04-14 23:44:54 +0530

    all user me console me data to aarha hai but display na hone filter ka error hai usko resolve krna padega

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -43,7 +43,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
-const allowedOrigins = ["http://localhost:3000", "http://localhost:8080"];
+const allowedOrigins = ["http://localhost:3000", "http://localhost:8080", "http://localhost:5173"];
 
 app.use(
   cors({
